c7c42271848db73fb668051a6c26075fa6ffd19c
colenelson0 <nel21048@byui.edu>
2024-09-26 20:17:37 -0600

    Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool playAgain;
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            string input;
+            int guess;
+            int guessCount = 0;
+            do
+            {
+                Console.Write("What is your guess? ");
+                input = Console.ReadLine();
+                guess = int.Parse(input);
+
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+
+                guessCount++;
+            } while (guess != magicNumber);
+
+            Console.WriteLine($"Total guesses: {guessCount}");
+
+            Console.Write("Play again? ");
+            input = Console.ReadLine();
+            playAgain = input == "yes";
+        } while (playAgain);
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        List<int> numberList;
+        numberList = new(); // creates the list during run time
+
+        int input = -1;
+        while (input != 0)
+        {
+            Console.Write("Enter something: ");
+            string inputString = Console.ReadLine();
+            input = int.Parse(inputString);
+            if (input != 0)
+            {
+                numberList.Add(input);
+            }
+        }
+
+        foreach (int item in numberList)
+        {
+            Console.WriteLine(item);
+        }
     }
 }
