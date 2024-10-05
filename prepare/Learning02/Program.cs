57d425e185d238e353a0ed5a657f6a40ecd36ecd
colenelson0 <nel21048@byui.edu>
2024-10-04 20:57:08 -0600

    Resume Abstraction

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Resume resume = new();
+
+        resume._name = "John Person";
+
+        Job job1 = new();
+        job1._company = "Company A";
+        job1._jobTitle = "Superstar";
+        job1._startYear = 2020;
+        job1._endYear = 2021;
+        resume._jobs.Add(job1);
+
+        Job job2 = new();
+        job2._company = "Company B";
+        job2._jobTitle = "Supermodel";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+        resume._jobs.Add(job2);
+
+        resume.DisplayResume();
     }
 }
